54e090a5589ef88ab4f3f09c5410067fe23f5700
rwbfd <ran.wang.math@gmail.com>
2020-09-13 16:49:31 +0800

    Added ingress tutorial

---
+++ geek_tutorial/ingress/apple/app/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_docker():
+	return "Hello Apple!\n"
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=3000)
